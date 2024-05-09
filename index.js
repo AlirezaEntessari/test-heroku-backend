8aa0d518934ad856d8c23944525214d7238efbfe
Alireza Entessari <alintssr@gmail.com>
2024-05-09 17:35:52 -0400

    added database url environment variable

--- index.js
+++ index.js
@@ -7,6 +7,28 @@ const pool = require('./db');
 app.use(cors());
 app.use(express.json()); // Add this line to parse JSON request bodies
 
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
+
+
+
+
 app.get("/test", async (req, res) => {
     try {
       const { rows, fields } = await pool.query("SELECT * FROM test_table;");
