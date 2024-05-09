341df85c03c58dc870d8a36da2b751de47b7fa65
Alireza Entessari <alintssr@gmail.com>
2024-05-09 17:00:44 -0400

    commented out table

--- index.js
+++ index.js
@@ -7,16 +7,16 @@ const pool = require('./db');
 app.use(cors());
 app.use(express.json()); // Add this line to parse JSON request bodies
 
-app.get("/test", async (req, res) => {
-    try {
-      const { rows, fields } = await pool.query("SELECT * FROM test_table;");
-      const columnNames = fields.map((field) => field.name);
-      res.json({ columnNames, rows });
-    } catch (error) {
-      console.error("Error fetching table data:", error);
-      res.status(500).json({ error: "Internal server error" });
-    }
-  });
+// app.get("/test", async (req, res) => {
+//     try {
+//       const { rows, fields } = await pool.query("SELECT * FROM test_table;");
+//       const columnNames = fields.map((field) => field.name);
+//       res.json({ columnNames, rows });
+//     } catch (error) {
+//       console.error("Error fetching table data:", error);
+//       res.status(500).json({ error: "Internal server error" });
+//     }
+//   });
 
   app.get("/test-2", async (req, res) => {
     try {
